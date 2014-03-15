5080cd60d6b7243622c56e78b1728f32322cec26
Frank Denis <github@pureftpd.org>
2014-03-15 00:41:07 -0700

    Use RETVAL_*, not RETURN_* when cleanup is needed.

--- libsodium.c
+++ libsodium.c
@@ -338,8 +338,8 @@ PHP_FUNCTION(crypto_secretbox_open)
                               nonce, key) != 0) {
         efree(out);
         RETURN_FALSE;
-    } else {        
-        RETURN_STRINGL((char *) out + crypto_secretbox_ZEROBYTES,
+    } else {
+        RETVAL_STRINGL((char *) out + crypto_secretbox_ZEROBYTES,
                        ciphertext_len - crypto_secretbox_MACBYTES, 1);
         efree(out);
     }
@@ -583,7 +583,7 @@ PHP_FUNCTION(crypto_box_open)
         efree(out);
         RETURN_FALSE;
     } else {
-        RETURN_STRINGL((char *) out + crypto_box_ZEROBYTES,
+        RETVAL_STRINGL((char *) out + crypto_box_ZEROBYTES,
                        ciphertext_len - crypto_box_MACBYTES, 1);
         efree(out);
     }
