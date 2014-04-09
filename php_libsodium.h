451002a731a48078f5f7694f21ab4a44175e515a
Frank Denis <github@pureftpd.org>
2014-04-09 14:22:48 -0700

    Indent / remove bloat

--- php_libsodium.h
+++ php_libsodium.h
@@ -1,22 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2013 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id$ */
 
 #ifndef PHP_LIBSODIUM_H
 #define PHP_LIBSODIUM_H
@@ -24,14 +5,14 @@
 extern zend_module_entry libsodium_module_entry;
 #define phpext_libsodium_ptr &libsodium_module_entry
 
-#define PHP_LIBSODIUM_VERSION "0.1.0" /* Replace with version number for your extension */
+#define PHP_LIBSODIUM_VERSION "0.1.1"
 
 #ifdef PHP_WIN32
-#       define PHP_LIBSODIUM_API __declspec(dllexport)
+# define PHP_LIBSODIUM_API __declspec(dllexport)
 #elif defined(__GNUC__) && __GNUC__ >= 4
-#       define PHP_LIBSODIUM_API __attribute__ ((visibility("default")))
+# define PHP_LIBSODIUM_API __attribute__ ((visibility("default")))
 #else
-#       define PHP_LIBSODIUM_API
+# define PHP_LIBSODIUM_API
 #endif
 
 #ifdef ZTS
