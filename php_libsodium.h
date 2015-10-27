b11a037e152b61db931040bab19d8c6360ed5f06
Frank Denis <github@pureftpd.org>
2015-10-27 10:25:39 +0100

    1.0.2

--- php_libsodium.h
+++ php_libsodium.h
@@ -5,7 +5,7 @@
 extern zend_module_entry libsodium_module_entry;
 #define phpext_libsodium_ptr &libsodium_module_entry
 
-#define PHP_LIBSODIUM_VERSION "1.0.1"
+#define PHP_LIBSODIUM_VERSION "1.0.2"
 
 #ifdef PHP_WIN32
 # define PHP_LIBSODIUM_API __declspec(dllexport)
