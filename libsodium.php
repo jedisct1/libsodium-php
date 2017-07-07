a430425d92838267272501908aae603f3fd2377e
Padraig Doran <padraigdoran@users.noreply.github.com>
2017-07-07 10:34:49 +0100

    Fix indentation

--- libsodium.php
+++ libsodium.php
@@ -2,7 +2,7 @@
 $br = (php_sapi_name() == "cli")? "":"<br>";
 
 if(!extension_loaded('libsodium')) {
-        dl('libsodium.' . PHP_SHLIB_SUFFIX);
+    dl('libsodium.' . PHP_SHLIB_SUFFIX);
 }
 $module = 'libsodium';
 $functions = get_extension_funcs($module);
@@ -14,10 +14,10 @@ echo "$br\n";
 $function = 'sodium_memzero';
 $exit = 0;
 if (extension_loaded($module)) {
-        $str = $function($module);
+    $str = $function($module);
 } else {
-        $str = "Module $module is not compiled into PHP";
-        $exit = 255;
+    $str = "Module $module is not compiled into PHP";
+    $exit = 255;
 }
 echo "$str\n";
 exit($exit);
