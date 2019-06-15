349b4cd1cd6ca73c91b9a30e8904dceec12e26d7
Frank Denis <github@pureftpd.org>
2019-06-15 18:02:39 +0200

    table_header() -> table_row()

--- libsodium.c
+++ libsodium.c
@@ -674,9 +674,9 @@ PHP_MINFO_FUNCTION(sodium)
 {
     php_info_print_table_start();
     php_info_print_table_header(2, "sodium support", "enabled");
-    php_info_print_table_header(2, "sodium compiled version", PHP_SODIUM_VERSION);
-    php_info_print_table_header(2, "libsodium headers version", SODIUM_VERSION_STRING);
-    php_info_print_table_header(2, "libsodium library version", sodium_version_string());
+    php_info_print_table_row(2, "sodium compiled version", PHP_SODIUM_VERSION);
+    php_info_print_table_row(2, "libsodium headers version", SODIUM_VERSION_STRING);
+    php_info_print_table_row(2, "libsodium library version", sodium_version_string());
     php_info_print_table_end();
 }
 
